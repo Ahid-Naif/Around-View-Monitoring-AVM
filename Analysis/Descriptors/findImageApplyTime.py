ddb4457bbcf8d3135298089fda4f95a1ef466427
Ahid-Naif <ahedqaood19@gmail.com>
2018-09-15 21:34:03 +0800

    fix code
    Former-commit-id: 4982ebead78bfea659659edd159f000c5613a227
    Former-commit-id: 9a7882ea129ef179822ad8207e0999d4d8f1517e

--- Analysis/Descriptors/findImageApplyTime.py
+++ Analysis/Descriptors/findImageApplyTime.py
@@ -1,25 +1,20 @@
 # the purpose of this code is to calculate the time needed to
 # apply the descriptors on different sets of images
 from __future__ import print_function
-import argparse
 from imutils import paths
 from imutils.video import FPS
 import cv2
 from myPackage.KeyPointDetectors import detectKeypoints
 from myPackage.KeyPointsDescriptors import computeKeypoints
-
-ap = argparse.ArgumentParser()
-ap.add_argument("-d", "--dataset", required=True, help="Path to the image")
-args = vars(ap.parse_args())
+import glob
 
 totalTime = 0
 for x in range(0,99):
-    imagePaths = list(paths.list_images(args["dataset"]))
     subTotalTime = 0
-    for (i, imagePath) in enumerate(imagePaths):
-        filename = imagePath[imagePath.rfind("/") + 1:]
-        image = cv2.imread(imagePath)
+    for filePath in glob.glob("dataset/Images/Random/*"):
 
+        image = cv2.imread(filePath)
+        
         ORB = detectKeypoints(image)
         (outputImage, keypoints) = ORB.detectORB(True)
         fps = FPS().start()
