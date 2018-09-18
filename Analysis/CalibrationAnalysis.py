bb05e50f7a50f3244290f4b8224822ae5046afe6
Ahid-Naif <ahedqaood19@gmail.com>
2018-09-18 18:08:57 +0800

    define calibrate()
    Former-commit-id: f1d68a285572ba42c67d686950561da479cc29f5
    Former-commit-id: 5e3d6727563f6c55e59b884cd2378933cc08c1a2

--- Analysis/CalibrationAnalysis.py
+++ Analysis/CalibrationAnalysis.py
@@ -4,3 +4,6 @@ from myPackage.Calibration import calibrate
 rawImage = cv2.imread("FrontRaw")
 
 calibratedImage = calibrate(rawImage)
+
+cv2.imshow("Calibrated Image", calibratedImage)
+cv2.waitKey()
