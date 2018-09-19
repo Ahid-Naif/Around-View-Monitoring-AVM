22e6e3a9cd1737519f0f0ef0380019f097d51d61
Ahid-Naif <ahedqaood19@gmail.com>
2018-09-19 13:24:36 +0800

    trying to solve an issue
    Former-commit-id: be034545211299a6aed766b3e3e59a3d17dee8eb
    Former-commit-id: e95f8d92cc29c4bae434d2b92db39d69a04ccce8

--- myPackage/Calibration.py
+++ myPackage/Calibration.py
@@ -3,24 +3,23 @@ import numpy as np
 
 def calibrate(img):
 
-    H11 = 0
-    H12 = 0
-    H13 = 0
-    H21 = 0
-    H22 = 0
-    H23 = 0
-    H31 = 0
-    H32 = 0
-    H33 = 0
+    H11 = 1
+    H12 = 2
+    H13 = 3
+    H21 = 4
+    H22 = 5
+    H23 = 6
+    H31 = 7
+    H32 = 8
+    H33 = 9
 
     K = np.array([[H11], [H12], [H13],
                   [H21], [H22], [H23],
                   [H31], [H32], [H33]])
 
-    D = np.array(0.0, 0.0, 0.0, 0.0) # ignore distortion vector for the moment
+    D = np.array([0.0, 0.0, 0.0, 0.0]) # ignore distortion vector for the moment
 
-    Knew = K.copy()
-    Knew[(0,1), (0,1)] *= 0.4 # what is this line for?
+    Knew = 0.4 * np.identity(3, dtype=float)
 
     undistortedImage = cv2.fisheye.undistortImage(img, K, D=D, Knew=Knew) # how does work?
 
