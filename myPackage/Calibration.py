e59b40d5e9c3600b39c8e59de83511553dd65cb6
Ahid-Naif <ahedqaood19@gmail.com>
2018-09-21 03:22:22 +0800

    update values of K matrix
    Former-commit-id: 99f4609f70ba09cc124d14129778bebbbce47c65
    Former-commit-id: 9511aefbcf68babc7cbac6001e0710701d843ba8

--- myPackage/Calibration.py
+++ myPackage/Calibration.py
@@ -3,23 +3,23 @@ import numpy as np
 
 def calibrate(img):
 
-    H11 = 1
-    H12 = 2
-    H13 = 3
-    H21 = 4
-    H22 = 5
-    H23 = 6
-    H31 = 7
-    H32 = 8
-    H33 = 9
+    H11 = 2350.6
+    H12 = 0
+    H13 = 314.3
+    H21 = 0
+    H22 = 237.5
+    H23 = 233.2
+    H31 = 0.0
+    H32 = 0.0
+    H33 = 1.0
 
     K = np.array([[H11, H12, H13],
                   [H21, H22, H23],
                   [H31, H32, H33]])
 
-    D = np.array([0.0, 0.0, 0.0, 0.0]) # ignore distortion vector for the moment
+    D = np.array([[0.01535], [-0.1092], [0.1003], [-0.03689]]) # ignore distortion vector for the moment
 
-    Knew = 0.4 * np.identity(3, dtype=float)
+    Knew = np.identity(3, dtype=float)
 
     undistortedImage = cv2.fisheye.undistortImage(img, K, D=D, Knew=Knew) # how does work?
 
