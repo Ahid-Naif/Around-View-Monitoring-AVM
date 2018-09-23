2854e25a2d50fc1e62f04c15d53c45ff75384dd0
Ahid-Naif <ahedqaood19@gmail.com>
2018-09-24 03:28:25 +0800

    create code that take snapshots to collect a set of images for the calibration process
    Former-commit-id: ebe1b4f9cd38fad7df1325c691d13758613d0c9c
    Former-commit-id: beec1633393bee7b238fec11acc35fc48cf46f11

---
+++ Analysis/takeSnapshots.py
@@ -0,0 +1,17 @@
+import cv2
+
+video = cv2.VideoCapture(0)
+
+counter = 0
+while True:
+    isGrabbed, frame = video.read()
+    if not isGrabbed:
+        break
+
+    cv2.imshow("Video", frame)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
+        break
+    elif key == ord("s"):
+        counter += 1
+        cv2.imwrite("image_no_"+str(counter)+".jpg", frame)
