5c1c5beae8de3df24c2f7e97820dd9d54e2594cb
Ahid-Naif <ahedqaood19@gmail.com>
2018-09-25 03:18:37 +0800

    chane calibrate() function location from FisheyeCalibration class into UndidtortFisheye class
    Former-commit-id: 556354b1fed3d094ffd088d4eb4f087da633ab10
    Former-commit-id: b166fadc5006fa3a57be0d6343a95d51d8dbc204

--- myPackage/FisheyeCalibration.py
+++ myPackage/FisheyeCalibration.py
@@ -77,27 +77,4 @@ class FisheyeCalibration:
 
         file.close()
 
-    def calibrate(self, img):
-
-        H11 = 2350.6
-        H12 = 0
-        H13 = 314.3
-        H21 = 0
-        H22 = 237.5
-        H23 = 233.2
-        H31 = 0.0
-        H32 = 0.0
-        H33 = 1.0
-
-        K = np.array([[H11, H12, H13],
-                    [H21, H22, H23],
-                    [H31, H32, H33]])
-
-        D = np.array([[0.01535], [-0.1092], [0.1003], [-0.03689]]) # ignore distortion vector for the moment
-
-        Knew = np.identity(3, dtype=float)
-
-        undistortedImage = cv2.fisheye.undistortImage(img, K, D=D, Knew=Knew) # how does work?
-
-        return undistortedImage
 
