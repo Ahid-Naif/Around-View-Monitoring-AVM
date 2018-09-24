c7eb1d73ec3f8d2f2603c2bb6430ec827559826f
Ahid-Naif <ahedqaood19@gmail.com>
2018-09-25 01:02:38 +0800

    reorganize the code
    Former-commit-id: e1e0961e87ac6e6700d97e3fba8c95219465f668
    Former-commit-id: c1560d01eccb3165ee7a4f1d5568dc06f0d0e651

--- myPackage/Calibration.py
+++ myPackage/Calibration.py
@@ -22,30 +22,7 @@ class FisheyeCalibration:
         self.D = np.zeros((4,1))
         self.numPoints = 0
         self.calibrationFlags = cv2.fisheye.CALIB_RECOMPUTE_EXTRINSIC + cv2.fisheye.CALIB_CHECK_COND +cv2.fisheye.CALIB_FIX_SKEW
-    def calibrate(self, img):
-
-        H11 = 2350.6
-        H12 = 0
-        H13 = 314.3
-        H21 = 0
-        H22 = 237.5
-        H23 = 233.2
-        H31 = 0.0
-        H32 = 0.0
-        H33 = 1.0
-
-        K = np.array([[H11, H12, H13],
-                    [H21, H22, H23],
-                    [H31, H32, H33]])
-
-        D = np.array([[0.01535], [-0.1092], [0.1003], [-0.03689]]) # ignore distortion vector for the moment
-
-        Knew = np.identity(3, dtype=float)
-
-        undistortedImage = cv2.fisheye.undistortImage(img, K, D=D, Knew=Knew) # how does work?
-
-        return undistortedImage
-
+    
     def processFrame(self, image):
         if self.imageShape == None:
             self.imageShape = image.shape[0:2]
@@ -88,3 +65,28 @@ class FisheyeCalibration:
         file.write("DIM=" + str(self.imageShape[::-1])+"\n")
         file.write("K= (" + str(self.K.tolist()) + ")\n")
         file.write("D= (" + str(self.D.tolist()) + ")\n")
+
+    def calibrate(self, img):
+
+        H11 = 2350.6
+        H12 = 0
+        H13 = 314.3
+        H21 = 0
+        H22 = 237.5
+        H23 = 233.2
+        H31 = 0.0
+        H32 = 0.0
+        H33 = 1.0
+
+        K = np.array([[H11, H12, H13],
+                    [H21, H22, H23],
+                    [H31, H32, H33]])
+
+        D = np.array([[0.01535], [-0.1092], [0.1003], [-0.03689]]) # ignore distortion vector for the moment
+
+        Knew = np.identity(3, dtype=float)
+
+        undistortedImage = cv2.fisheye.undistortImage(img, K, D=D, Knew=Knew) # how does work?
+
+        return undistortedImage
+
