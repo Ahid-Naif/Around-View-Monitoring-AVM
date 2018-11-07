ee980e6a0df74d1cd64fd276c90c401a2aabff10
Ahid-Naif <ahedqaood19@gmail.com>
2018-11-08 04:47:33 +0800

    minor changes
    Former-commit-id: 4f0a8ceeb452284c956643d9847045185d38c9c9
    Former-commit-id: a75d1420cab87086f23f35495b08089458c0d2ba

--- Analysis/undistortImage.py
+++ Analysis/undistortImage.py
@@ -1,25 +1,28 @@
 import cv2
 import numpy as np
 from myPackage.UndistortFisheye import UndistortFisheye
-from myPackage.myTrackBar import customizedTrackBar
+from myPackage.customizedTrackBar import customizedTrackBar
 import glob
 
-balanceTrackBar = customizedTrackBar(0, 0.1, 1, 0, "balance", "Tuning")
+# balanceTrackBar = customizedTrackBar(0, 0.1, 1, 0, "balance", "Tuning")
 
 undistortFisheye = UndistortFisheye()
 
 while True:
-    image = cv2.imread("RawImage2.jpg")
+    image = cv2.imread("raw.jpg")
 
-    balance = balanceTrackBar.getValue()
+    # balance = balanceTrackBar.getValue()
 
     clearImage1 = undistortFisheye.undistort(image)
-    clearImage2 = undistortFisheye.undistort2(image, balance=balance)
+    clearImage3 = undistortFisheye.undistort3(image)
 
-    clearImages = np.hstack((clearImage1, clearImage2))
-    cv2.imshow("Undistorted", clearImages)
+    # clearImages = np.hstack((clearImage1, clearImage2))
+    cv2.imshow("Undistorted", clearImage1)
+    cv2.imshow("Undistorted3", clearImage3)
     key = cv2.waitKey(1) & 0xFF
     if key == ord("q"):
         break
+    if key == ord("r"):
+        undistortFisheye.reset()
 
 cv2.destroyAllWindows()
