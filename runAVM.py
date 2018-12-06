c0b4e8f51fd229aa8dd676edf018e752768fc07f
Ahid-Naif <ahedqaood19@gmail.com>
2018-12-07 01:23:40 +0800

    create the main code that utilizes AroundViewMonitoring class
    Former-commit-id: 4e7c7bffc28ab64032590202140237bd24352ec5
    Former-commit-id: 5210f1f674f395a99c3e1dfd859c2c347a9c9bef

--- runAVM.py
+++ runAVM.py
@@ -1,14 +1,30 @@
 import cv2
 from Camera.AroundViewMonitoring import avm
+import datetime
 
-frontStream = cv2.VideoCapture(0)
-backStream = cv2.VideoCapture(0)
+
+# frontStream = cv2.VideoCapture(0)
+# backStream = cv2.VideoCapture(1)
+frontStream = cv2.VideoCapture("front_camera.avi")
+backStream = cv2.VideoCapture("back_camera.avi")
 
 avm = avm()
 
-frontFrame = cv2.imread("Front_View.jpg")
-backFrame = cv2.imread("Back_View.jpg")
-frontView, backView = avm.runAVM(frontFrame, backFrame)
-cv2.imshow("Front Bird's Eye View", frontView)
-cv2.imshow("Back Bird's Eye View", backView)
-cv2.waitKey()
+# frontFrame = cv2.imread("Front_View.jpg")
+# backFrame = cv2.imread("Back_View.jpg")
+startTime = datetime.datetime.now()
+while True:
+    isGrabbed, frontFrame = frontStream.read()
+    isGrabbed2, backFrame = backStream.read()
+    if not isGrabbed or not isGrabbed2:
+        break
+
+    birdView = avm.runAVM(frontFrame, backFrame)
+    cv2.imshow("Bird's Eye View", birdView)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
+        break
+endTime = datetime.datetime.now()
+time = (endTime - startTime).total_seconds()
+print ("Approximate elapsed time is %(fps)d: "%{"fps": time})
+cv2.destroyAllWindows()
