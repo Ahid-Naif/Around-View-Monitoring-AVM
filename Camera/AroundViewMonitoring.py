7c5d172be31958fd859cb91b3a1cb20a976d8dfc
Ahid-Naif <ahedqaood19@gmail.com>
2018-12-07 01:24:49 +0800

    create AroudViewMonitoring class which contains all other classes that were made to make AVM system work
    Former-commit-id: 61d41c4042433944e2bc7d9aaf687aa4c3a1d094
    Former-commit-id: 247918e3271d275fc8b8ba6fababcf8ccb654fdd

--- Camera/AroundViewMonitoring.py
+++ Camera/AroundViewMonitoring.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import imutils
 from Camera.Undistortion import UndistortFisheye
 from Camera.PerspectiveTransformation import EagleView
 
@@ -10,8 +11,8 @@ class avm:
 
         self.__frontEagle = EagleView()
         self.__backEagle = EagleView()
-        self.__frontEagle.setDimensions((188, 197), (478, 207), (579, 395), (96, 362))
-        self.__backEagle.setDimensions((171, 240), (469, 240), (603, 452), (52, 441))
+        self.__frontEagle.setDimensions((149, 195), (439, 207), (528, 380), (37, 374))
+        self.__backEagle.setDimensions((164, 229), (469, 229), (588, 430), (45, 435))
     
     def runAVM(self, frontFrame, backFrame):
         frontView = self.__frontCamera.undistort(frontFrame)
@@ -19,4 +20,37 @@ class avm:
         backView = self.__backCamera.undistort(backFrame)
         topDown_Back = self.__backEagle.transfrom(backView)
 
+        topDown_Front , topDown_Back = self.__reScale(topDown_Front, topDown_Back)
+        middleView = self.__getMiddleView(topDown_Front)
+        birdView = np.vstack((topDown_Front, middleView, topDown_Back))
+        return birdView
+    
+    def __reScale(self, topDown_Front, topDown_Back):
+        width_FrontView = topDown_Front.shape[1]
+        width_BackView = topDown_Back.shape[1]
+        height_FrontView = topDown_Front.shape[0]
+        height_BackView = topDown_Back.shape[0]
+
+        if width_FrontView > width_BackView:
+            newWidth = width_BackView
+            ratio = width_BackView/width_FrontView
+            newHeight = int(ratio * height_FrontView)
+            topDown_Front = cv2.resize(topDown_Front, (newWidth, newHeight))
+        else:
+            newWidth = width_FrontView
+            ratio = width_FrontView/width_BackView
+            newHeight = int(ratio * height_BackView)
+            topDown_Back = cv2.resize(topDown_Back, (newWidth, newHeight))
+        
         return topDown_Front, topDown_Back
+    
+    def __getMiddleView(self, topDown_Front):
+        # the length of the image represents the distance in front or back of the car
+        height_FrontView = topDown_Front.shape[0]
+        
+        realHeight_FrontView = 13 # unit is cm
+        realHeight_MiddleView = 29.5 # unit is cm
+        height_MiddleView = int(realHeight_MiddleView/realHeight_FrontView * height_FrontView)
+        width_MiddleView = int(topDown_Front.shape[1])  
+        middleView = np.zeros((height_MiddleView, width_MiddleView, 3), np.uint8)
+        return middleView
