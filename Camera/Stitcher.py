eac8e3863b1bbb864fe425969dcbd6cfbdd4e9bc
Ahid-Naif <ahedqaood19@gmail.com>
2018-12-16 05:48:13 +0800

    images to stitch can be updated whenever stitch() is called
    Former-commit-id: c03104e5ea60450ed6be6ea4fcbd6a42b3a4f70e
    Former-commit-id: 066193d58109b8b2ff86101bdf46dcd8eb2a4629

--- Camera/Stitcher.py
+++ Camera/Stitcher.py
@@ -4,20 +4,18 @@ from FeatureExtraction.KeyPointDetectors import detectKeypoints
 from FeatureExtraction.KeyPointsDescriptors import computeKeypoints
 
 class stitchTwoImages:
-    def __init__(self, image1, image2, orientation):
-        self.image1 = image1
-        self.image2 = image2
+    def __init__(self, orientation):
         self.orientation = orientation
 
         self.kpsDetector = detectKeypoints()
         self.kpsDescriptor = computeKeypoints()
 
-    def stitch(self):
-        kps1 = self.kpsDetector.detectORB(self.image1)
-        kps1, des1 = self.kpsDescriptor.computeORB(self.image1, kps1)
+    def stitch(self, image1, image2):
+        kps1 = self.kpsDetector.detectORB(image1)
+        kps1, des1 = self.kpsDescriptor.computeORB(image1, kps1)
 
-        kps2 = self.kpsDetector.detectORB(self.image2)
-        kps2, des2 = self.kpsDescriptor.computeORB(self.image2, kps2)
+        kps2 = self.kpsDetector.detectORB(image2)
+        kps2, des2 = self.kpsDescriptor.computeORB(image2, kps2)
 
         # convert the keypoints from KeyPoint objects to NumPy
         # arrays
@@ -25,14 +23,16 @@ class stitchTwoImages:
         Kps2 = np.float32([kp2.pt for kp2 in kps2])
 
         _, homographyMatrix, _ = self.__matchKeypoints(Kps1, Kps2, des1, des2)
-
-        newHeight, newWidth = self.__getNewDimensions(homographyMatrix)
-        
+        width_1 = image1.shape[1]
+        height_1 = image1.shape[0]
+        width_2 = image2.shape[1]
+        height_2 = image2.shape[0]
+        newHeight, newWidth = self.__getNewDimensions(homographyMatrix, width_1, height_1, width_2, height_2)
         
         # warp the img1 which is on the right/down
-        result = cv2.warpPerspective(self.image1, homographyMatrix, (newWidth, newHeight))
+        result = cv2.warpPerspective(image1, homographyMatrix, (newWidth, newHeight))
 
-        result[0:self.image2.shape[0], 0:self.image2.shape[1]] = self.image2
+        result[0:height_2, 0:width_2] = image2
         return result
     
     def __matchKeypoints(self, kps1, kps2, des1, des2):
@@ -54,19 +54,19 @@ class stitchTwoImages:
         else:
             return None
 
-    def __getNewDimensions(self, M):
-        x = self.image1.shape[1]
-        y = self.image1.shape[0]
+    def __getNewDimensions(self, M, width_1, height_1, width_2, height_2):
+        x = width_1
+        y = height_1
         newX = (x * M[0][0] + y * M[0][1] + M[0][2]) / (x * M[2][0] + y * M[2][1] + M[2][2])
         newY = (x * M[1][0] + y * M[1][1] + M[1][2]) / (x * M[2][0] + y * M[2][1] + M[2][2])
 
         if self.orientation == "Right2Left":
-            newHeight = self.image1.shape[0]
-            newWidth = self.image2.shape[1] + self.image2.shape[1]
+            newHeight = height_1
+            newWidth = width_1 + width_2
             newWidth -= (newWidth - int(newX))
         elif self.orientation == "Bottom2Upper":
-            newHeight = self.image1.shape[0] + self.image2.shape[0]
+            newHeight = height_1 + height_2
             newHeight -= (newHeight - int(newY))
-            newWidth = self.image1.shape[1]
+            newWidth = width_1
         
         return newHeight , newWidth 
